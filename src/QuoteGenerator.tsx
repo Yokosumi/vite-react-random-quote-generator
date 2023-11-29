12d9c79c6ac95fc749c7da0275c9b224e2991479
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-11-29 16:19:59 +0100

    Quote generator works

--- src/QuoteGenerator.tsx
+++ src/QuoteGenerator.tsx
@@ -1,7 +1,7 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react'
+import axios from "axios"
+import { useEffect, useState } from "react"
 
-const quotesURL = 'https://api.quotable.io/random'
+const quotesURL = "https://api.quotable.io/random"
 
 type QuoteTypes = {
     content: string
@@ -19,9 +19,13 @@ export const QuoteGenerator = () => {
             const quotes = await response.data
             if (response.status === 200) {
                 setRandomQuotes(quotes)
+            } else if (response.status === 429) {
+                console.log("Error: rate limit exceeded")
+            } else {
+                console.log(`Error: ${JSON.stringify(response.status)}`)
             }
         } catch (error) {
-            console.log(`Oh Oh! Something went wrong: {error}`)
+            console.log(`Oh Oh! Something went wrong: ${JSON.stringify(error)}`)
         }
     }
 
@@ -30,7 +34,17 @@ export const QuoteGenerator = () => {
     }, [])
     return (
         <>
-            <p>{randomQuotes.content}</p>
+            {/*TODO: center card & fix and style button*/}
+            <div className="flex justify-center">
+                <div className="m-4 flex items-center max-w-sm min-w-[30rem] min-h-[10rem] p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 ">
+                    <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"></h2>
+                    <p className="font-normal text-gray-700 dark:text-gray-400">
+                        " {randomQuotes.content} " -{" "}
+                        <span className="italic">{randomQuotes.author}</span>
+                    </p>
+                </div>
+                <button onClick={updateQuote}>New Quote</button>
+            </div>
         </>
     )
 }
